1e5b52d4546b067483ba9d7d2918aa44ede4041e
Tom Quiggle <tquiggle@gmail.com>
2026-09-10 11:39:57 -0700

    Enlarge logo size

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../img/logo.svg';
+import logo from '../img/logo.png';
 import { Navbar, Provider, Container } from 'rendition';
 import { NetworkInfoForm } from './NetworkInfoForm';
 import { Notifications } from './Notifications';
@@ -81,7 +81,7 @@ const App = () => {
 	return (
 		<Provider>
 			<GlobalStyle />
-			<Navbar brand={<img src={logo} style={{ height: 30 }} alt="logo" />} />
+			<Navbar brand={<img src={logo} style={{ height: 60 }} alt="logo" />} />
 
 			<Container>
 				<Notifications
